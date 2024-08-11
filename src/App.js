5e3d7808cf618c8cf903bc233d67fa5aeb37eab3
Rohit kushwaha <kushwaharohit967@gmail.com>
2024-08-11 23:50:51 +0530

    commit my

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomeComponents from "./Components/HomeComponents";
+import IciciThanks from "./Components/IciciThanks";
+import ContentICICI from "./Components/ContentICICI";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeComponents />} />
+        <Route path="/IciciThanks" element={<IciciThanks />} />
+        <Route path="/ContentICICI" element={<ContentICICI />} />
+
+      </Routes>
+    </Router>
   );
 }
 
